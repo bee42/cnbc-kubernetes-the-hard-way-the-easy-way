162a8615207f57763844236a8769c7d270086376
Peter Rossbach <peter.rossbach@bee42.com>
2025-04-23 23:34:17 +0200

    Fix: heredoc
    Signed-off-by: Peter Rossbach <peter.rossbach@bee42.com>

--- 00-certificates/02-kubelet-client/generate-kubelet-certificates.sh
+++ 00-certificates/02-kubelet-client/generate-kubelet-certificates.sh
@@ -24,44 +24,45 @@ fi
 
 for instance in "${MINIONS[@]}"; do
 
-  echo "Generating kubelet node client and server certificate for ${instance}"
+echo "Generating kubelet node client and server certificate for ${instance}"
 
-  if [[ "${MULTIPASS_ENABLED}" == 'on' ]] ; then
-    INTERNAL_IP="$(multipass info "${instance}" | grep 'IPv4' | awk '{ print $2 }')"
-  else
-    INTERNAL_IP=$(ssh "${instance}" "hostname -I | awk '{print \$1}'")
-  fi
-  cat > "${instance}"-csr.json <<EOF
-  {
-    "CN": "system:node:${instance}",
-    "key": {
-      "algo": "rsa",
-      "size": 2048
-    },
-    "hosts": [
-      "${instance}",
-      "${INTERNAL_IP}"
-    ],
-    "names": [
-      {
-        "C": "${COUNTRY}",
-        "L": "${CITY}",
-        "O": "system:nodes",
-        "OU": "Kubernetes The Hard Way",
-        "ST": "${STATE}"
-      }
-    ]
-  }
-  EOF
+if [[ "${MULTIPASS_ENABLED}" == 'on' ]] ; then
+  INTERNAL_IP="$(multipass info "${instance}" | grep 'IPv4' | awk '{ print $2 }')"
+else
+  INTERNAL_IP=$(ssh "${instance}" "hostname -I | awk '{print \$1}'")
+fi
 
-  # Generate the kubelet client and server certificate
-  # The certificate is signed by the Kubernetes CA
-  # The certificate is used by the kubelet to authenticate to the Kubernetes API server
-  cfssl gencert \
-    -ca=../00-Certificate-Authority/kubernetes-ca.pem \
-    -ca-key=../00-Certificate-Authority/kubernetes-ca-key.pem \
-    -config=../00-Certificate-Authority/kubernetes-ca-config.json \
-    -profile=worker \
-    "${instance}"-csr.json | cfssljson -bare "${instance}"
+cat > "${instance}"-csr.json <<EOF
+{
+  "CN": "system:node:${instance}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "hosts": [
+    "${instance}",
+    "${INTERNAL_IP}"
+  ],
+  "names": [
+    {
+      "C": "${COUNTRY}",
+      "L": "${CITY}",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "${STATE}"
+    }
+  ]
+}
+EOF
+
+# Generate the kubelet client and server certificate
+# The certificate is signed by the Kubernetes CA
+# The certificate is used by the kubelet to authenticate to the Kubernetes API server
+cfssl gencert \
+  -ca=../00-Certificate-Authority/kubernetes-ca.pem \
+  -ca-key=../00-Certificate-Authority/kubernetes-ca-key.pem \
+  -config=../00-Certificate-Authority/kubernetes-ca-config.json \
+  -profile=worker \
+  "${instance}"-csr.json | cfssljson -bare "${instance}"
 
 done
